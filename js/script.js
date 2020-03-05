2493305780dc6f11999f8329b27506f364136a13
samuelpacheco270 <sp6236@thewcs.org>
2020-03-05 23:03:21 +0000

    i got my work to show

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-let titles = [];
-titles.forEach(function(title){
-$(".songs").append("<div>")   
-})
+// let titles = [];
+// titles.forEach(function(title){
+// $(".songs").append("<div>")   
+// })
+let save = []
+
+$(".button").click(function(){
+  let artist =  $(".artist").val();
+  let song =  $(".song").val();
+  let duration =  $(".duration").val();
+  let photo = $(".photo").val();
+  let link= $(".link").val();
+
+ let obj = {
+     artist,
+     song,
+     duration,
+     photo,
+     link,
+ }
+ save.push(obj);
+
+ $(".playlist").empty();
+save.forEach(function(info){
+    $(".playlist").append("<p>Artist: "+ info.artist +"</p>")
+    $(".playlist").append("<p>Song: "+ info.song +"</p>")
+    $(".playlist").append("<p>"+ info.duration +"</p>")
+    $(".playlist").append("<p>"+ info.photo +"</p>")
+    $(".playlist").append("<p>"+ info.link +"</p>")
+});
+    //$(".playlist").text(artist+song)
+});
